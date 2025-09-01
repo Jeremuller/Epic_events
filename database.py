19dad8e3e3ef7fff2a6464f3f955e925451b982f
Jeremy Muller <jeremymuller92@gmail.com>
2025-09-01 16:28:45 +0200

    Updating database.py to keep DATABASE_URL datas safe

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL =
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
