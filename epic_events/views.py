eff8cd23cd8aac45ad2d371b1582e0678c025358
Jeremy Muller <jeremymuller92@gmail.com>
2025-11-02 12:43:30 +0100

    Started refactoring the error messages system by using a python Enum class.
    Class is declared, get function too, next step is to try it in views/models functions.

--- epic_events/views.py
+++ epic_events/views.py
@@ -3,6 +3,33 @@ from epic_events.models import User, Client, Contract, Event
 from epic_events.database import SessionLocal
 from sqlalchemy.exc import OperationalError, ProgrammingError, InternalError
 from datetime import datetime
+from enum import Enum
+
+
+class ErrorMessages(Enum):
+    USERNAME_TAKEN = "This username is already taken."
+    EMAIL_TAKEN = "This email is already registered."
+    REQUIRED_FIELDS_EMPTY = "Required fields cannot be empty or whitespace."
+    INVALID_ROLE = "Invalid role. Must be one of: commercial, management, support."
+    DELETE_FAILED = "Failed to delete user. Dependencies may be locked."
+    DATABASE_ERROR = "A technical error occurred. Please try again later."
+    CONTACT_NOT_FOUND = "The contact mentioned does not exist."
+    INFERIOR_TOTAL_PRICE = "Total price can't be inferior to rest to pay."
+    INVALID_TOTAL_PRICE = "Total_price can't be <= 0."
+    NEGATIVE_REST_TO_PAY = "Rest to pay can't be < 0."
+    CLIENT_NOT_FOUND = "The specified client does not exist."
+    CONTRACT_NOT_FOUND = "The specified contract does not exist."
+    EVENT_DATE_IN_PAST = "Event date must be in the future."
+    END_BEFORE_START = "End date must be after start date."
+
+    @classmethod
+    def get_message(cls, error_key):
+        """Return error message associated to error key."""
+        try:
+            return cls[error_key].value
+        except KeyError:
+            return cls.DATABASE_ERROR.value
+
 
 ERROR_MESSAGES = {
     "username_taken": "This username is already taken.",
