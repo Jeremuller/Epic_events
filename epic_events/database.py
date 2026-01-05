753263a2b5da48bf1a4ea9c56d39ab6d5821016f
Jeremy Muller <jeremymuller92@gmail.com>
2026-01-05 14:25:07 +0100

    Fixed import in database.py

--- epic_events/database.py
+++ epic_events/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+
+from epic_events.models import Base
 import os
 
 load_dotenv()
@@ -8,12 +10,18 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = (f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
                            f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def get_engine():
+    return create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+engine = get_engine()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def init_db():
+    """Create all database tables."""
     print("Database initialization in progress...")
     Base.metadata.create_all(bind=engine)
     print("Tables successfully created. ✅ ")
